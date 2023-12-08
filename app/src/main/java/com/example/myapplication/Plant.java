bd002ac903d8e8e3275d8ccae1420518d46ac1bd
sqwertyl <andrewchen118@gmail.com>
2023-12-08 02:44:33 -0800

    added plant height field & descriptor in plant object

--- app/src/main/java/com/example/myapplication/Plant.java
+++ app/src/main/java/com/example/myapplication/Plant.java
@@ -5,6 +5,8 @@ public class Plant {
     private String info;
     private String plantId;
     private String imageURL;
+    private String currentHeight;
+    private String previousHeights;
     private static final int imageResId = R.drawable.ic_launcher_foreground;
 
     /*
@@ -28,10 +30,14 @@ public class Plant {
 //    public int getImageResId() { return imageResId; }
     public String getPlantId() { return plantId; }
     public String getImageURL() { return imageURL; }
+    public String getCurrentHeight() { return currentHeight; }
+    public String getPreviousHeights() { return previousHeights; }
 
     public void setTitle(String title) { this.title = title; }
     public void setInfo(String info) { this.info = info; }
     public void setPlantId(String id) { plantId = id; }
     public void setImageURL(String newImageURL) { imageURL = newImageURL; }
+    public void setCurrentHeight(String height) { currentHeight = height; }
+    public void setPreviousHeights(String heights) { previousHeights = heights; }
     public Boolean hasImage() { return imageURL != null; }
 }
